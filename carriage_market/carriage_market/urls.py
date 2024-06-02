a94969d448c8ab3cec3dccfab05ba32ca8f7a8b1
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2024-06-02 17:30:22 +0200

    installed react and created first page using react with django

--- carriage_market/carriage_market/urls.py
+++ carriage_market/carriage_market/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
 ]
 
 if settings.DEBUG:
